1970ad476662e86eabe76550379d6b4e33e4aad2
Idsrathor <167456599+Idsrathor@users.noreply.github.com>
2024-04-21 15:29:57 +0530

    Update mp3.py

--- mp3.py
+++ mp3.py
@@ -11,14 +11,16 @@ def main():
     st.title("MP3 Music Player")
 
     # List of MP3 files (you can customize this)
-    mp3_files = [
-        "my_queen.mp3",
-        "kale_kagaz.mp3",
-        "matak_chalungi.mp3",
-        "Bajao_Dhol_Swagat_Me.mp3",
-        "Har_Har_Shambhu.mp3"        
-        # Add more songs here
-    ]
+    mp3_files = ['Humare_Saath_Shri_Raghunath.mp3',
+                 'Har_Har_Shambhu.mp3',
+                 'Bajao_Dhol_Swagat_Me.mp3',
+                 'Achutam_keshavam.mp3',
+                 'yam_yam_yaksharupam_mahakal bhairav strotam.mp3',
+                 'shiv_tandav_strot.mp3',
+                 'my_queen.mp3',
+                 'Meri_Jhopdi_Ke_Bhag_Aaj_Khul_Jayenge.mp3',
+                 'matak_chalungi.mp3',
+                 'kale_kagaz.mp3']
 
     selected_song = st.selectbox("Select a song", mp3_files)
 
